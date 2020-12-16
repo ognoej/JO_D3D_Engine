6f5df556aa8a1221624fea0318931e81e4592c8f
no in <ognoej@gmail.com>
2020-12-16 16:18:46 +0900

    exeption fixed

--- JO_Dx11_Engine/MyErrorCheck.h
+++ JO_Dx11_Engine/MyErrorCheck.h
@@ -4,19 +4,19 @@
 #include <Windows.h>
 #include <string>
 
-#define COM_ERROR_IF_FAILED( hr, msg ) if( FAILED( hr ) ) throw MyErrorCheck( hr, msg )
+#define COM_ERROR_IF_FAILED( hr, msg ) if( FAILED( hr ) ) throw MyErrorCheck( hr, msg)//, __FILE__, __FUNCTION__, __LINE__  )
 
 class MyErrorCheck
 {
 public:
-	MyErrorCheck(HRESULT hr, const std::string& msg, const std::string& file, const std::string& function, int line)
+	MyErrorCheck(HRESULT hr, const std::string& msg)//, const std::string& file, const std::string& function, int line)
 	{
 		_com_error error(hr);
 		whatmsg = L"Msg: " + MyString::toWstring(std::string(msg)) + L"\n";
 		whatmsg += error.ErrorMessage();
-		whatmsg += L"\nFile: " + MyString::toWstring(file);
-		whatmsg += L"\nFunction: " + MyString::toWstring(function);
-		whatmsg += L"\nLine: " + MyString::toWstring(std::to_string(line));
+		//whatmsg += L"\nFile: " + MyString::toWstring(file);
+		//whatmsg += L"\nFunction: " + MyString::toWstring(function);
+		//whatmsg += L"\nLine: " + MyString::toWstring(std::to_string(line));
 	}
 
 public:
