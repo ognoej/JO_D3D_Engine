0651242b799f489c48877529a02eccc21267f6de
ognoej <ognoej@gmail.com>
2021-05-14 15:21:28 +0900

    D3D11effect.h added

---
+++ JO_Dx11_Engine/EffectMgr.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <d3d11.h>
+#include <Windows.h>
+#define SafeDelete(x) { delete x; x = 0; }
+#include <string>
+#include "d3d11effect.h"
+
+class EffectMgr
+{
+public:
+	EffectMgr(ID3D11Device* device, const std::wstring& filename);
+	virtual ~EffectMgr();
+
+private:
+	EffectMgr(const EffectMgr& rhs);
+	EffectMgr& operator=(const EffectMgr& rhs);
+
+protected:
+	ID3DX11Effect* mFX;
+
+};
+
